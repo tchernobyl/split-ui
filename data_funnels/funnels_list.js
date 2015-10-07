ddda395a6388ee3dfd5452560f2b192b56341b34
ameur hamdouni <ameur.hamdouni@gmail.com>
2015-10-07 12:19:35 +0100

    create other funnels module

---
+++ data_funnels/funnels_list.js
@@ -0,0 +1,284 @@
+var funnels_list =
+    [
+        {
+            id: 1,
+            name: 'funnel_1',
+            steps: [
+                {
+                    id: 1,
+                    name: 'f1_step_1',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f1_s1_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f1_s1_experience_2'
+                        }
+                    ]
+
+                },
+                {
+                    id: 2,
+                    name: 'f1_step_2',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f1_s2_experience_1'
+                        }
+                    ]
+
+                },
+                {
+                    id: 3,
+                    name: 'f1_step_3',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f1_s3_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f1_s3_experience_2'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: 'funnel_2',
+            steps: [
+                {
+                    id: 1,
+                    name: 'f2_step_1',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f2_s1_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f2_s1_experience_2'
+                        },
+                        {
+                            id: 3,
+                            name: 'f2_s1_experience_3'
+                        }
+
+                    ]
+
+                },
+                {
+                    id: 2,
+                    name: 'f2_step_2',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f2_s2_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f2_s2_experience_2'
+                        }
+                    ]
+
+                },
+                {
+                    id: 3,
+                    name: 'f2_step_3',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f2_s3_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f2_s3_experience_2'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            id: 3,
+            name: 'funnel_3',
+            steps: [
+                {
+                    id: 1,
+                    name: 'f3_step_1',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f3_s1_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f3_s1_experience_2'
+                        }
+                    ]
+                },
+                {
+                    id: 2,
+                    name: 'f3_step_2',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f3_s2_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f3_s2_experience_2'
+                        },
+                        {
+                            id: 3,
+                            name: 'f3_s2_experience_3'
+                        }
+                    ]
+                },
+                {
+                    id: 3,
+                    name: 'f3_step_3',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f3_s3_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f3_s3_experience_2'
+                        }
+                    ]
+                },
+                {
+                    id: 4,
+                    name: 'f3_step_4',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f3_s4_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f3_s4_experience_2'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            id: 4,
+            name: 'funnel_4',
+            steps: [
+                {
+                    id: 1,
+                    name: 'f4_step_1',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f4_s1_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f4_s1_experience_2'
+                        }
+                    ]
+                },
+                {
+                    id: 2,
+                    name: 'f4_step_2',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f4_s2_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f4_s2_experience_2'
+                        },
+                        {
+                            id: 3,
+                            name: 'f4_s2_experience_3'
+                        }
+                    ]
+                },
+                {
+                    id: 3,
+                    name: 'f4_step_3',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f4_s3_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f4_s3_experience_2'
+                        }
+                    ]
+                },
+                {
+                    id: 4,
+                    name: 'f4_step_4',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f4_s4_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f4_s4_experience_2'
+                        }
+                    ]
+                },
+
+
+                {
+                    id: 5,
+                    name: 'f4_step_5',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f4_s5_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f4_s5_experience_2'
+                        },
+                        {
+                            id: 3,
+                            name: 'f4_s5_experience_3'
+                        }
+                    ]
+                },
+                {
+                    id: 6,
+                    name: 'f4_step_6',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f4_s6_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f4_s6_experience_2'
+                        }
+                    ]
+                },
+                {
+                    id: 7,
+                    name: 'f4_step_7',
+                    experiences: [
+                        {
+                            id: 1,
+                            name: 'f4_s7_experience_1'
+                        },
+                        {
+                            id: 2,
+                            name: 'f4_s7_experience_2'
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
